2738bd5f208921bf538671a253bb702975688017
Eduardo Pagel <pagel.edu@gmail.com>
2025-12-10 12:00:20 -0300

    feat: implementação do auth

--- src/tema/services/tema.service.ts
+++ src/tema/services/tema.service.ts
@@ -1,63 +1,63 @@
-import { HttpException, HttpStatus, Injectable } from "@nestjs/common";
-import { DeleteResult, ILike, Repository } from "typeorm";
-import { Tema } from "../entities/tema.entity";
-import { InjectRepository } from "@nestjs/typeorm";
+import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
+import { DeleteResult, ILike, Repository } from 'typeorm';
+import { Tema } from '../entities/tema.entity';
+import { InjectRepository } from '@nestjs/typeorm';
 
 @Injectable()
 export class TemaService {
-    constructor(
-        @InjectRepository(Tema)
-        private temaRepository: Repository<Tema>,
-    ) {}
+  constructor(
+    @InjectRepository(Tema)
+    private temaRepository: Repository<Tema>,
+  ) {}
 
-    async findAll(): Promise<Tema[]> {
-        return await this.temaRepository.find({
-            relations: {
-                postagem: true
-            },
-        });
+  async findAll(): Promise<Tema[]> {
+    return await this.temaRepository.find({
+      relations: {
+        postagem: true,
+      },
+    });
+  }
+
+  async findById(id: number): Promise<Tema> {
+    const tema = await this.temaRepository.findOne({
+      where: {
+        id,
+      },
+      relations: {
+        postagem: true,
+      },
+    });
+
+    if (!tema) {
+      throw new HttpException('Tema não encontrado!', HttpStatus.NOT_FOUND);
     }
+    return tema;
+  }
 
-    async findById(id: number): Promise<Tema> {
-        const tema = await this.temaRepository.findOne({
-            where: {
-                id,
-            },
-            relations: {
-                postagem: true,
-            },
-        });
+  async findAllByDescricao(descricao: string): Promise<Tema[]> {
+    return await this.temaRepository.find({
+      where: {
+        descricao: ILike(`%${descricao}%`),
+      },
+      relations: {
+        postagem: true,
+      },
+    });
+  }
 
-        if(!tema) {
-            throw new HttpException('Tema não encontrado!', HttpStatus.NOT_FOUND);
-        }
-        return tema;
-    }
+  async create(Tema: Tema): Promise<Tema> {
+    return await this.temaRepository.save(Tema);
+  }
 
-    async findAllByDescricao(descricao: string): Promise<Tema[]> {
-        return await this.temaRepository.find({
-            where: {
-                descricao: ILike(`%${descricao}%`)
-            },
-            relations: {
-                postagem: true
-            },
-        })
-    }
+  async update(tema: Tema): Promise<Tema> {
+    await this.findById(tema.id);
 
-    async create(Tema: Tema): Promise<Tema> {
-        return await this.temaRepository.save(Tema);
-    }
+    return await this.temaRepository.save(tema);
+  }
 
-    async update(tema: Tema): Promise<Tema> {
-        await this.findById(tema.id);
+  async delete(id: number): Promise<DeleteResult> {
+    await this.findById(id);
 
-        return await this.temaRepository.save(tema);
-    }
-
-    async delete(id: number): Promise<DeleteResult> {
-        await this.findById(id);
-
-        return await this.temaRepository.delete(id);
-    }
+    return await this.temaRepository.delete(id);
+  }
 }
